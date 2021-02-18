dba9aa7fc678bbbb1d4692303f45b9d44ae69b6d
The Noise <leespaulding25@gmail.com>
2021-02-18 14:59:35 -0500

    added propmt function and questions for user to answer

--- utils/index.js
+++ utils/index.js
@@ -2,8 +2,66 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const generateReadme = require("./utils/generateReadme");
 
 // Write File Async
 const writeFileAsync = util.promisify(fs.writeFile);
 
+// Created an array of questions for user input and generate in Readme.md
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type:"input",
+            name:"github info",
+            message:"What is your Github Username?"   
+        },
+        {
+            type:"input",
+            name:"useremail",
+            message:"What is you Email?"   
+        },
+        {
+            type:"input",
+            name:"nameofproject",
+            message:"What is your projects name?"   
+        },
+        {
+            type:"input",
+            name:"projecdescription",
+            message:"Please write a short description of your project:"   
+        },
+        {
+            type:"input",
+            name:"licensing",
+            message:"what kind of license should your project have?"   
+        }, {
+            type:"input",
+            name:"dependecies",
+            message:"What command should run to install dependencies?"   
+        },
+        {
+            type:"input",
+            name:"Testing",
+            message:"What command should be run to test?"   
+        },
+        {
+            type:"input",
+            name:"repoinfo",
+            message:"What dose the user need to know using your repo?"   
+        },
+        {
+            type:"input",
+            name:"participation",
+            message:"What dose the user need to know about contributing to the repo?"   
+        },
+    ]);    
+}
+// Async function using util.promisify to resolve Promise
+//using try to test block as it runs..
+async function init(){
+    try{
+        
 
+    }
+
+}
