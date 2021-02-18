bd989cfac621897c1fcce44f5340c3771518a339
The Noise <leespaulding25@gmail.com>
2021-02-18 14:06:53 -0500

    added required packages and file Asyncto index.js

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,9 @@
-const questions = [
-    
-]
+// Packages used for application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+// Write File Async
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
